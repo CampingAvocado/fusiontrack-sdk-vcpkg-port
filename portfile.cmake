936ca1503561b0a17233302969ae34348159db2d
Manuel Saladin <Manuel.Saladin@balgrist.ch>
2024-10-17 12:54:10 +0200

    inital portfile and config

---
+++ portfile.cmake
@@ -0,0 +1,44 @@
+vcpkg_check_linkage()
+set(VCPKG_BUILD_TYPE release)
+
+# Download the library - Skip for now because Windows has an installer
+# if(VCPKG_TARGET_IS_WINDOWS)
+#     vcpkg_download_distfile(ARCHIVE
+#         URLS "https://example.com/path/to/library/windows.zip"
+#         FILENAME "library-windows.zip"
+#         SHA512 <SHA512_HASH>
+#     )
+# elseif(VCPKG_TARGET_IS_LINUX)
+#     vcpkg_download_distfile(ARCHIVE
+#         URLS "https://example.com/path/to/library/linux.tar.gz"
+#         FILENAME "library-linux.tar.gz"
+#         SHA512 <SHA512_HASH>
+#     )
+# endif()
+
+# Extract the library
+# vcpkg_extract_source_archive_ex(
+#     OUT_SOURCE_PATH SOURCE_PATH
+#     ARCHIVE "${ARCHIVE}"
+# )
+
+cmake_path(SET SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR}) # TEMP
+
+# Copy headers
+file(GLOB headers "${SOURCE_PATH}/include/*" "${SOURCE_PATH}/samples/*.hpp") # todo; are samples even needed?
+file(COPY ${headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(COPY "${SOURCE_PATH}/atnet" DESTINATION "${CURRENT_PACKAGES_DIR}/include/atnet") # copy atnet headers into subdir to preserve
+                                                                                      # '#include' syntax from standalone library
+
+# Copy binaries
+if(VCPKG_TARGET_IS_WINDOWS)
+    file(COPY "${SOURCE_PATH}/lib/" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+    file(COPY "${SOURCE_PATH}/bin/" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+elseif(VCPKG_TARGET_IS_LINUX) # TODO: rectify
+endif()
+
+# Copy sources - this is not normally done but the partially compiled mess that is the Atracsys SDK forces my hands here...
+file(COPY "${SOURCE_PATH}/advancedAPI/" DESTINATION "${CURRENT_PACKAGES_DIR}/src/advancedAPI")
+
+# Copy CMake config files
+file(COPY "${SOURCE_PATH}/cmake/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
